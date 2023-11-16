8c2080a94a869129f835a8ea93a4854a200b0283
RKabir <taposkabir@yahoo.com>
2023-11-16 21:28:52 +0600

    email vlidation helper created for registration

--- routes/api/authRoutes.js
+++ routes/api/authRoutes.js
@@ -1,7 +1,24 @@
 const express = require("express")
 const _ = express.Router()
+const emailValidation = require("../../helpers/emailValidation")
 
 
-_.get("/registration", function(req,res){res.send("registration done")})
+_.post("/registration", function(req,res){
+    let {name, email, password} = req.body
+    if(!name){
+        res.send("Enter Name")
+    }else if(!email){
+        res.send("Enter Email")
+    }else if(!password){
+        res.send("Enter Password")
+    }else{
+        if(email){
+            if(!emailValidation(email)){
+                return res.send("Invalid Email")
+            }
+        }
+        res.send("Registration Done")
+    }
+})
 
 module.exports = _;
