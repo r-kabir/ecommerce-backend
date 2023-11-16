4292401da0bc79803d23e921adba335ed345ce7c
RKabir <taposkabir@yahoo.com>
2023-11-16 23:07:21 +0600

    registration restricted for existing email user

--- controllers/registrationController.js
+++ controllers/registrationController.js
@@ -1,30 +1,35 @@
 const emailValidation = require("../helpers/emailValidation")
 const User = require("../models/userSchema")
 
-let registrationController = (req,res)=>{
+let registrationController = async (req,res)=>{
     let {name, email, password} = req.body
-    if(!name){
-        res.send("Enter Name")
-    }else if(!email){
-        res.send("Enter Email")
-    }else if(!password){
-        res.send("Enter Password")
-    }else{
-        if(email){
-            if(!emailValidation(email)){
-                return res.send("Invalid Email")
+    let existingUser = await User.find({email:email})
+    if(existingUser.length == 0){
+        if(!name){
+            res.send("Enter Name")
+        }else if(!email){
+            res.send("Enter Email")
+        }else if(!password){
+            res.send("Enter Password")
+        }else{
+            if(email){
+                if(!emailValidation(email)){
+                    return res.send("Invalid Email")
+                }
             }
+            let user = new User({
+                name: name,
+                email: email,
+                password: password
+    
+            })
+            user.save()
+    
+            res.send(user)
         }
-        let user = new User({
-            name: name,
-            email: email,
-            password: password
-
-        })
-        user.save()
-
-        res.send(user)
-    }
+    }else{
+        res.send("Email Already Exists")
+    }   
 }
 
 
